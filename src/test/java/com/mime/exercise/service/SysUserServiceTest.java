eea761ca96c180bf2ad624b11bd0aaf544cb45a8
ninaime <488548018@qq.com>
2019-02-28 00:52:28 +0800

    1.添加JDBC六个步骤

--- src/test/java/com/mime/exercise/service/SysUserServiceTest.java
+++ src/test/java/com/mime/exercise/service/SysUserServiceTest.java
@@ -5,8 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SysUserServiceTest {
 	
+	private static BeanFactory beanFactory;
+
 	public static void main(String[] args) {
-		//1.加载spring容器，即加载并且解析applicationContext.xml
-		BeanFactory beanFactory = new ClassPathXmlApplicationContext("");
+		beanFactory = new ClassPathXmlApplicationContext("config/applicationContext-construction.xml");
+		//2.spring容器中获取sysUserService对象
+		SysUserService sysUserService = (SysUserService)beanFactory.getBean("sysUserService");
+		//调用sysUserService中的测试方法
+		sysUserService.selectUser();
+		
 	}
 }
